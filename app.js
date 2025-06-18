598a50db0c098847b0a24eff5aa668d037af1f22
Deepanshi004 <144414748+Deepanshi004@users.noreply.github.com>
2025-06-18 16:47:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const schoolRoutes = require('./routes/schoolRoutes');
 const app = express();
 app.use(express.json());
 
-app.use('/', schoolRoutes);
+app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
